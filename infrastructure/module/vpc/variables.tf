d08da13588599a092dd2155c7c5739226751f2ac
Sergei Khazov <serkhazov@gmail.com>
2024-06-15 04:23:36 -0400

    add VPC and subnets

---
+++ infrastructure/module/vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "cidr_block" {
+  type        = string
+  description = "VPC CIDR Block"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_ip_on_launch" {
+  type        = bool
+  description = "Assign Public IP for new instances launched into that subnet"
+  default     = true
+}
